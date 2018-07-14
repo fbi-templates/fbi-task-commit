74d8d7314d7ab7a7a08c4955e79e74cfd81c98a9
neikvon <neikvon@hotmail.com>
2018-07-14 23:11:38 +0800

    feat: first release

---
+++ README.md
@@ -0,0 +1,43 @@
+# fbi-task-commit
+git commit and npm publish flow, formatting commit messages with commitizen.
+
+> This is a fbi task. If you haven't installed [fbi](https://github.com/AlloyTeam/fbi) yet, use the following command to install.
+>
+> `$ npm i -g fbi` or `yarn global add fbi`
+
+## Requirements
+- `fbi v3.0+`
+- `node v7.6+`
+
+## Features
+
+- select files for `git add`
+- formatting commit messages with commitizen
+- bump version
+- `git push`
+- `npm publish`
+
+## Usage
+
+**Install**
+
+```bash
+$ fbi add https://github.com/fbi-templates/fbi-task-commit.git
+```
+
+**Run**
+
+```bash
+$ cd path/to/git/repository
+$ fbi commit
+```
+
+
+## More
+- [Official templates](https://github.com/fbi-templates)
+- [`fbi` documentation](https://neikvon.gitbooks.io/fbi/content/)
+
+## License
+[MIT](https://opensource.org/licenses/MIT)
+
+## [Changelog](https://github.com/fbi-templates/fbi-task-commit/CHANGELOG.md)
---
+++ index.js
@@ -0,0 +1,93 @@
+const inquirer = require('inquirer')
+const version = require('./lib/version')
+const commit = require('./lib/commit')
+const publish = require('./lib/publish')
+const {
+  isGitRepo,
+  getStatus,
+  getUnpushedCommits,
+  push,
+  gitInit
+} = require('./lib/git')
+
+process.on('unhandledRejection', (reason, promise) => {
+  ctx.logger.error(
+    'Unhandled Rejection at: Promise ',
+    promise,
+    ' reason: ',
+    reason
+  )
+  throw reason
+})
+
+process.on('uncaughtException', async error => {
+  ctx.logger.error(error)
+  process.exit(0)
+})
+
+const defaults = {
+  repoPath: process.cwd()
+}
+
+async function entry (options = defaults) {
+  // prevent additional parameters results in an git git error
+  process.argv = process.argv.slice(0, 3)
+
+  if (!await isGitRepo(options.repoPath)) {
+    await gitInit()
+  }
+
+  try {
+    const hadCommited = await commit(options)
+    if (hadCommited) {
+      ctx.logger.success('Selected files committed\n')
+    }
+
+    // bump version
+    await version.bump()
+
+    // push
+    const unPushed = await getUnpushedCommits()
+    if (unPushed) {
+      console.log()
+      ctx.logger.info(
+        `Unpushed commits(${unPushed.split('\n').filter(u => u).length}):`
+      )
+      console.log(unPushed)
+
+      const answer = await inquirer.prompt({
+        type: 'confirm',
+        name: 'pushCommits',
+        message: 'Do you want to push now?',
+        default: false
+      })
+
+      if (answer.pushCommits) {
+        await push()
+        await push('--tags')
+        ctx.logger.success('All commits and tags pushed\n')
+      }
+    }
+
+    // publish
+    if (hadCommited) {
+      await publish()
+      ctx.logger.success('Publish successfully\n')
+    }
+
+    // status
+    await getStatus()
+    const unPushed2 = await getUnpushedCommits()
+    if (unPushed2) {
+      console.log(
+        ` (${unPushed2.split('\n').filter(u => u).length} commits unpushed)`
+      )
+    }
+    console.log()
+  } catch (err) {
+    ctx.logger.error(err)
+    process.exit(0)
+  }
+}
+
+module.exports = entry
