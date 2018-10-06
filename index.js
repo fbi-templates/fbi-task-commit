418a215e06ffacd0cafa5dcbbab72655b7c5f6ca
neikvon <neikvon@hotmail.com>
2018-10-06 11:02:26 +0800

    feat: new flow

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const inquirer = require('inquirer')
-const version = require('./lib/version')
-const commit = require('./lib/commit')
-const publish = require('./lib/publish')
-const {
-  isGitRepo,
-  getStatus,
-  getUnpushedCommits,
-  push,
-  gitInit
-} = require('./lib/git')
+const chalk = require('chalk')
+const flows = require('./lib/flows/')
+const status = require('./lib/helpers/status')
+const utils = require('./lib/helpers/utils')
+let configs
 
 process.on('unhandledRejection', (reason, promise) => {
   ctx.logger.error(
@@ -25,65 +20,211 @@ process.on('uncaughtException', async error => {
   process.exit(0)
 })
 
-const defaults = {
-  repoPath: process.cwd()
-}
+async function initConfigs () {
+  const defConfigs = require('./configs.json')
+  const userConfigs = ctx && ctx.options ? ctx.options.commit : null
+  const flowNames = Object.keys(defConfigs.flow)
+  const configs = JSON.parse(JSON.stringify(defConfigs))
 
-async function entry (options = defaults) {
-  // prevent additional parameters results in an git git error
-  process.argv = process.argv.slice(0, 3)
-
-  if (!await isGitRepo(options.repoPath)) {
-    await gitInit()
-  }
-
-  try {
-    const hadCommited = await commit(options)
-    if (hadCommited) {
-      ctx.logger.success('Selected files committed\n')
-    }
-
-    // bump version
-    await version.bump()
-
-    // push
-    const unPushed = await getUnpushedCommits()
-    if (unPushed) {
-      console.log()
-      ctx.logger.info(
-        `Unpushed commits(${unPushed.split('\n').filter(u => u).length}):`
-      )
-      console.log(unPushed)
-
-      const answer = await inquirer.prompt({
-        type: 'confirm',
-        name: 'pushCommits',
-        message: 'Do you want to push now?',
-        default: false
-      })
-
-      if (answer.pushCommits) {
-        await push()
-        await push('--tags')
-        ctx.logger.success('All commits and tags pushed\n')
+  if (userConfigs) {
+    if (userConfigs.flow) {
+      if (
+        typeof userConfigs.flow === 'string' &&
+        flowNames.includes(userConfigs.flow)
+      ) {
+        configs.flow = {
+          ...configs.flow[userConfigs.flow],
+          name: userConfigs.flow
+        }
+      } else if (
+        userConfigs.flow.name &&
+        flowNames.includes(userConfigs.flow)
+      ) {
+        configs.flow = userConfigs.flow
+      } else {
+        console.log('config error')
+      }
+    } else {
+      configs.flow = {
+        ...configs.flow[flowNames[0]],
+        name: flowNames[0]
       }
     }
-
-    // publish
-    await publish()
-
-    // status
-    await getStatus()
-    const unPushed2 = await getUnpushedCommits()
-    if (unPushed2) {
-      console.log(
-        ` (${unPushed2.split('\n').filter(u => u).length} commits unpushed)`
-      )
+    configs['standard-version'] = Object.assign(
+      {},
+      configs['standard-version'],
+      userConfigs['standard-version'] || {}
+    )
+  } else {
+    configs.flow = {
+      ...configs.flow[flowNames[0]],
+      name: flowNames[0]
     }
-    console.log()
+  }
+
+  configs['root'] = await status.rootPath()
+
+  return configs
+}
+
+async function statusCheck () {
+  const next = await flows.check(configs)
+  if (next) {
+    await flows[next]()
+  } else {
+    await flows.commit()
+  }
+}
+
+async function mainFlow (configs, statusCheck) {
+  let choices
+  switch (configs.flow.name) {
+    case 'github':
+      choices = [
+        {
+          name: 'new branch',
+          value: 'new-branch'
+        },
+        {
+          name: 'sync branch',
+          value: 'sync-branch'
+        },
+        {
+          name: 'release',
+          value: 'release'
+        }
+      ]
+    case 'gitlab':
+    case 'gitflow':
+      choices = [
+        {
+          name: 'new branch',
+          value: 'new-branch'
+        },
+        {
+          name: 'sync branch',
+          value: 'sync-branch'
+        },
+        {
+          name: 'release pre-production',
+          value: 'release-pre-production'
+        },
+        {
+          name: 'release production',
+          value: 'release-production'
+        }
+      ]
+      break
+    default:
+      choices = [
+        {
+          name: 'sync branch',
+          value: 'sync-branch'
+        },
+        {
+          name: 'release',
+          value: 'release'
+        }
+      ]
+      break
+  }
+
+  choices = [
+    {
+      name: 'show status',
+      value: 'show-status'
+    },
+    {
+      name: 'commit',
+      value: 'commit'
+    }
+  ].concat(choices)
+  choices.push(new inquirer.Separator())
+  choices = choices.concat([
+    {
+      name: 'helpers',
+      value: 'helpers'
+    },
+    {
+      name: 'exit',
+      value: 'exit'
+    }
+  ])
+
+  console.log()
+  const { flowName, helper } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'flowName',
+      message: `${await utils.promptPrefix()}[${chalk.yellow(configs.flow.name + ' flow')}] Choose a action:`,
+      choices,
+      pageSize: 20
+    },
+    {
+      type: 'list',
+      name: 'helper',
+      message: 'Choose a helper',
+      when (answers) {
+        return answers.flowName === 'helpers'
+      },
+      choices: Object.keys(flows['helpers']).map(h => ({
+        name: h.replace(/-/g, ' '),
+        value: h
+      })),
+      pageSize: 20
+    }
+  ])
+
+  const actionName = helper ? `helpers:${helper}` : flowName
+  const fn = helper ? flows['helpers'][helper] : flows[actionName]
+
+  if (actionName === 'exit') {
+    return
+  }
+
+  if (actionName === 'commit') {
+    if (await status.canCommit(configs)) {
+      await fn()
+    } else {
+      console.log('nothing to commit')
+    }
+  } else {
+    await fn(configs, statusCheck)
+  }
+
+  console.log(chalk.green(`DONE: ${actionName.replace(/-/g, ' ')}`))
+
+  await mainFlow(configs, statusCheck)
+}
+
+const exec = require('./lib/helpers/exec')
+const execa = require('execa')
+
+async function entry () {
+  // const staleBranches = await status.staleBranches('feature/demo')
+  // console.log('staleBranches:', staleBranches)
+  // return
+
+  configs = await initConfigs()
+  try {
+    await flows.init(configs)
+    // await statusCheck()
+    await flows['show-status']()
+    // return
+
+    const next = await flows.check(configs)
+    if (next) {
+      await flows[next]()
+    }
+
+    if (await status.canCommit(configs)) {
+      await flows.commit()
+    }
+
+    await mainFlow(configs, statusCheck)
   } catch (err) {
-    ctx.logger.error(err)
-    process.exit(0)
+    console.error(chalk.red(err.message))
+    console.log(err)
   }
 }
 
---
+++ lib/flows/readme.md
@@ -0,0 +1,24 @@
+# Flows
+
+1. centralized
+
+   1. sync branch
+
+1. github-flow
+
+   1. new branch
+   1. sync branch
+   1. pull request
+
+1. gitlab-flow
+
+   1. new branch
+   1. sync branch
+   1. env
+   1. merge request
+   1. cherry-pick
+
+1. gitflow
+
+   1. new branch
+   1. sync branch
