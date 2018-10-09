ef1576140ad53ff6d82287f36429069c6a29605f
neikvon <neikvon@hotmail.com>
2018-10-10 00:38:00 +0800

    feat: stage

--- index.js
+++ index.js
@@ -1,166 +1,91 @@
-const inquirer = require('inquirer')
+const path = require('path')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 const flows = require('./lib/flows/')
 const status = require('./lib/helpers/status')
 const utils = require('./lib/helpers/utils')
+const exec = require('./lib/helpers/exec')
+const i18n = require('./lib/helpers/i18n')
+
 let configs
+const params = {
+  configs,
+  status,
+  utils,
+  inquirer,
+  chalk,
+  exec,
+  statusCheck
+}
 
-process.on('unhandledRejection', (reason, promise) => {
-  ctx.logger.error(
-    'Unhandled Rejection at: Promise ',
-    promise,
-    ' reason: ',
-    reason
-  )
-  throw reason
-})
-
-process.on('uncaughtException', async error => {
-  ctx.logger.error(error)
-  process.exit(0)
-})
-
-async function initConfigs () {
-  const defConfigs = require('./configs.json')
-  const userConfigs = ctx && ctx.options ? ctx.options.commit : null
-  const flowNames = Object.keys(defConfigs.flow)
-  const configs = JSON.parse(JSON.stringify(defConfigs))
-
-  if (userConfigs) {
-    if (userConfigs.flow) {
-      if (
-        typeof userConfigs.flow === 'string' &&
-        flowNames.includes(userConfigs.flow)
-      ) {
-        configs.flow = {
-          ...configs.flow[userConfigs.flow],
-          name: userConfigs.flow
-        }
-      } else if (
-        userConfigs.flow.name &&
-        flowNames.includes(userConfigs.flow)
-      ) {
-        configs.flow = userConfigs.flow
-      } else {
-        console.log('config error')
-      }
-    } else {
-      configs.flow = {
-        ...configs.flow[flowNames[0]],
-        name: flowNames[0]
-      }
-    }
-    configs['standard-version'] = Object.assign(
-      {},
-      configs['standard-version'],
-      userConfigs['standard-version'] || {}
-    )
-  } else {
-    configs.flow = {
-      ...configs.flow[flowNames[0]],
-      name: flowNames[0]
+function pathToFunction (obj, basePath) {
+  for (let [key, val] of Object.entries(obj)) {
+    try {
+      obj[key] = require(path.join(basePath || __dirname, val))
+    } catch (err) {
+      delete obj[key]
+      console.log(chalk.red(`Cannot find module "${val}"`))
     }
   }
+}
 
+const initConfigs = async () => {
+  const defConfigs = require('./configs')
+  pathToFunction(defConfigs.flows)
+  const userConfigs = ctx && ctx.options ? ctx.options.commit : {}
+  pathToFunction(userConfigs.flows || {}, process.cwd())
+
+  configs = Object.assign({}, defConfigs, userConfigs)
+  const allFlowsNames = Object.keys(configs.flows)
   configs['root'] = await status.rootPath()
+
+  const defName = 'No Flow'
+  let flowName = configs['flow-name'] || defName
+
+  if (!allFlowsNames.includes(flowName)) {
+    console.log(
+      chalk.yellow(
+        `\nConfigs of "${flowName}" not found, fallback to default "${defName}".`
+      )
+    )
+    flowName = defName
+  }
+  configs.flow = configs.flows[flowName]
+  configs['flow-name'] = flowName
 
   return configs
 }
 
 async function statusCheck () {
-  const next = await flows.check(configs)
+  const next = await flows.check(params)
   if (next) {
-    await flows[next]()
-  } else {
-    await flows.commit()
+    await flows[next](params)
   }
 }
 
-async function mainFlow (configs, statusCheck) {
-  let choices
-  switch (configs.flow.name) {
-    case 'github':
-      choices = [
-        {
-          name: 'new branch',
-          value: 'new-branch'
-        },
-        {
-          name: 'sync branch',
-          value: 'sync-branch'
-        },
-        {
-          name: 'release',
-          value: 'release'
-        }
-      ]
-    case 'gitlab':
-    case 'gitflow':
-      choices = [
-        {
-          name: 'new branch',
-          value: 'new-branch'
-        },
-        {
-          name: 'sync branch',
-          value: 'sync-branch'
-        },
-        {
-          name: 'release pre-production',
-          value: 'release-pre-production'
-        },
-        {
-          name: 'release production',
-          value: 'release-production'
-        }
-      ]
-      break
-    default:
-      choices = [
-        {
-          name: 'sync branch',
-          value: 'sync-branch'
-        },
-        {
-          name: 'release',
-          value: 'release'
-        }
-      ]
-      break
+async function main () {
+  const messagePrefix = `${await utils.promptPrefix()}[${chalk.yellow(configs['flow-name'])}]`
+
+  const actions = Object.keys(configs.flow)
+
+  if (configs.actions && configs.actions.pre) {
+    actions.unshift(...configs.actions.pre, new inquirer.Separator())
+  }
+  if (configs.actions && configs.actions.post) {
+    actions.push(new inquirer.Separator(), ...configs.actions.post)
   }
 
-  choices = [
-    {
-      name: 'show status',
-      value: 'show-status'
-    },
-    {
-      name: 'commit',
-      value: 'commit'
-    }
-  ].concat(choices)
-  choices.push(new inquirer.Separator())
-  choices = choices.concat([
-    {
-      name: 'helpers',
-      value: 'helpers'
-    },
-    {
-      name: 'exit',
-      value: 'exit'
-    }
-  ])
-
-  console.log()
-  const { flowName, helper } = await inquirer.prompt([
+  const prompts = [
     {
       type: 'list',
       name: 'flowName',
-      message: `${await utils.promptPrefix()}[${chalk.yellow(configs.flow.name + ' flow')}] Choose a action:`,
-      choices,
+      message: `${messagePrefix} Choose a action:`,
+      choices: actions,
       pageSize: 20
-    },
-    {
+    }
+  ]
+  if (configs.actions.post.includes('helpers')) {
+    prompts.push({
       type: 'list',
       name: 'helper',
       message: 'Choose a helper',
@@ -172,59 +97,44 @@ async function mainFlow (configs, statusCheck) {
         value: h
       })),
       pageSize: 20
-    }
-  ])
+    })
+  }
+  const { flowName, helper } = await inquirer.prompt(prompts)
 
   const actionName = helper ? `helpers:${helper}` : flowName
-  const fn = helper ? flows['helpers'][helper] : flows[actionName]
+  const fn = helper
+    ? flows['helpers'][helper]
+    : configs.flow[actionName] || flows[actionName]
 
-  if (actionName === 'exit') {
-    return
+  fn && (await fn(params))
+
+  if (configs.logs.DONE) {
+    console.log(chalk.green(`DONE: ${actionName.replace(/-/g, ' ')}\n`))
   }
 
-  if (actionName === 'commit') {
-    if (await status.canCommit(configs)) {
-      await fn()
-    } else {
-      console.log('nothing to commit')
-    }
-  } else {
-    await fn(configs, statusCheck)
-  }
-
-  console.log(chalk.green(`DONE: ${actionName.replace(/-/g, ' ')}`))
-
-  await mainFlow(configs, statusCheck)
+  await main()
 }
 
-const exec = require('./lib/helpers/exec')
-const execa = require('execa')
-
 async function entry () {
-  // const staleBranches = await status.staleBranches('feature/demo')
-  // console.log('staleBranches:', staleBranches)
-  // return
+  const t = await i18n()
+  utils['t'] = t
 
-  configs = await initConfigs()
-  try {
-    await flows.init(configs)
-    // await statusCheck()
-    await flows['show-status']()
-    // return
+  console.log(t('key'))
 
-    const next = await flows.check(configs)
-    if (next) {
-      await flows[next]()
-    }
+  if (!await status.isGitRepo()) {
+    return utils.gitInit()
+  }
 
-    if (await status.canCommit(configs)) {
-      await flows.commit()
-    }
+  await initConfigs()
 
-    await mainFlow(configs, statusCheck)
-  } catch (err) {
-    console.error(chalk.red(err.message))
-    console.log(err)
+  if (configs.actions.before && flows[configs.actions.before]) {
+    await flows[configs.actions.before](params)
+  }
+
+  await main()
+
+  if (configs.actions.after && flows[configs.actions.after]) {
+    await flows[configs.actions.after](params)
   }
 }
 
